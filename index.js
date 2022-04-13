2de2e22db79f720923552999b41e7fe0e276d89e
G Roques <g.roques@charter.com>
2022-04-13 13:39:45 -0500

    Complete problem

--- index.js
+++ index.js
@@ -1,57 +1,8 @@
-/*
-Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-
-Symbol       Value
-I             1
-V             5
-X             10
-L             50
-C             100
-D             500
-M             1000
-
-For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
-
-Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
-
-I can be placed before V (5) and X (10) to make 4 and 9. 
-X can be placed before L (50) and C (100) to make 40 and 90. 
-C can be placed before D (500) and M (1000) to make 400 and 900.
-Given a roman numeral, convert it to an integer.
-
-Example 1:
-
-Input: s = "III"
-Output: 3
-Explanation: III = 3.
-Example 2:
-
-Input: s = "LVIII"
-Output: 58
-Explanation: L = 50, V= 5, III = 3.
-Example 3:
-
-Input: s = "MCMXCIV"
-Output: 1994
-Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
- 
-
-Constraints:
-
-1 <= s.length <= 15
-s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
-It is guaranteed that s is a valid roman numeral in the range [1, 3999].
-*/
-
-function romanToInteger(roman) {
-    const characters = Array.from(roman);
-    console.log(characters)
-    return characters.reduce((sum, romanNumeral) => (
-        sum + integerByRomanNumeral[romanNumeral]
-    ), 0);
-}
-
-const integerByRomanNumeral = {
+/**
+ * Roman numerals are represented by seven different symbols:
+ * I, V, X, L, C, D and M.
+ */
+ const INTEGER_BY_ROMAN_NUMERAL = {
     I: 1,
     V: 5,
     X: 10,
@@ -60,5 +11,96 @@ const integerByRomanNumeral = {
     D: 500,
     M: 1000
 };
+
+/**
+ * There are six instances where subtraction is used:
+ *
+ * 1. I can be placed before V (5) and X (10) to make 4 and 9. 
+ * 2. X can be placed before L (50) and C (100) to make 40 and 90. 
+ * 3. C can be placed before D (500) and M (1000) to make 400 and 900.
+ */
+const SUBTRACTION_PATTERNS = new Set([
+    'IV',
+    'IX',
+    'XL',
+    'XC',
+    'CD',
+    'CM'
+]);
+
+/**
+ * Converts roman numerals to an integer.
+ * 
+ * @example
+ * romanToInteger('IV')
+ * 4
+ * 
+ * @param {string} roman String of roman numerals.
+ * @returns {number} Integer
+ */
+function romanToInteger(roman) {
+    const characters = Array.from(roman);
+    return characters.reduce((sum, romanNumeral, index) => {
+        const addOrSubtract = getAddOrSubtract(characters, index);
+        return addOrSubtract(sum, INTEGER_BY_ROMAN_NUMERAL[romanNumeral]);
+    }, 0);
+}
+
+/**
+ * Get whether to add or subtract the integer value for a roman numeral.
+ * 
+ * @param {Array} characters Array of roman numeral characters.
+ * @param {number} index Index of roman numeral to determine whether
+ *                       to add or subtract.
+ * @returns {Function} Function to add or subtract two numbers.
+ */
+function getAddOrSubtract(characters, index) {
+    // if there's only 1 character, or if it's last the character,
+    // then add it's value.
+    if (characters.length <= 1 || index === characters.length - 1) {
+        return add;
+    }
+    const windows = toWindows(characters, 2);
+    const window = windows[index];
+    const romanNumeral = window.join('');
+    const shouldSubtract = SUBTRACTION_PATTERNS.has(romanNumeral);
+    return shouldSubtract ? subtract : add;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+/**
+ * Converts an array into an array of windows.
+ * 
+ * @example
+ * toWindows([1, 2, 3, 4], 2)
+ * [[1, 2], [2, 3], [3, 4]]
+ * 
+ * @example
+ * toWindows([1], 2)
+ * []
+ * 
+ * @see {@link https://stackoverflow.com/a/59322890}
+ *
+ * @param {Array} array Array to create windows from.
+ * @param {number} windowSize Size of window.
+ * @returns Array of windows.
+ */
+function toWindows(array, windowSize) { 
+    return array.reduce((windows, _, index) => {
+        if (index + windowSize > array.length) {
+            return windows;
+        }
+        return windows.concat(
+            [array.slice(index, index + windowSize)]
+        );
+    }, []);
+}
 
 module.exports = romanToInteger;
--- test.js
+++ test.js
@@ -1,12 +1,25 @@
-var assert = require('assert');
+const assert = require('assert');
 const romanToInteger = require('./index');
 
-
 describe('romanToInteger', function () {
-    it('should return 1 for I', function () {
-        assert.equal(romanToInteger('I'), 1);
-    });
-    it('should return 2 for II', function () {
-        assert.equal(romanToInteger('II'), 2);
+    const tests = [
+        {roman: 'I', integer: 1},
+        {roman: 'II', integer: 2},
+        {roman: 'III', integer: 3},
+        {roman: 'IV', integer: 4},
+        {roman: 'V', integer: 5},
+        {roman: 'VI', integer: 6},
+        {roman: 'VII', integer: 7},
+        {roman: 'VIII', integer: 8},
+        {roman: 'IX', integer: 9},
+        {roman: 'X', integer: 10},
+        {roman: 'LVIII', integer: 58},
+        {roman: 'MCMXCIV', integer: 1994}
+    ];
+     
+    tests.forEach(({roman, integer}) => {
+        it(`should return ${integer} for ${roman}`, function () {
+            assert.equal(romanToInteger(roman), integer);
+        });
     });
 });
